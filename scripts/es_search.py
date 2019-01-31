85d72b9ced03df0825e1d5eb3f98d5af3e3981fe
EC2 Default User <ec2-user@ip-172-31-33-237.us-east-2.compute.internal>
2019-01-31 15:56:17 +0000

    Minor cleaning

--- scripts/es_search.py
+++ scripts/es_search.py
@@ -3,8 +3,6 @@ Using elasticsearch geo queries.
 https://www.elastic.co/guide/en/elasticsearch/reference/current/geo-queries.html
 '''
 
-from elasticsearch import Elasticsearch, RequestsHttpConnection
-from elasticsearch.helpers import bulk
 from requests_aws4auth import AWS4Auth
 
 import boto3
@@ -14,28 +12,10 @@ import requests
 # May be irrelevant for monuments, small things, etc.
 # https://www.elastic.co/guide/en/elasticsearch/reference/current/geo-shape.html
 
-# TODO: needs to handle aws auth. Currently, the es is setup to allow all
-# queries from this ip.
-
 HOST = 'https://search-freedom-locations-test-zj3i4hp466x4rddyw6wwgaaovm.us-east-1.es.amazonaws.com/'
 INDEX = 'parks'
 REGION = 'us-east-1'
-DOC_TYPE = '_doc'
 SERVICE = 'es'
-
-def client():
-    credentials = boto3.Session().get_credentials()
-    awsauth = AWS4Auth(
-            credentials.access_key, credentials.secret_key, REGION, SERVICE)
-
-    es = Elasticsearch(
-            hosts = [{'host': HOST, 'port': 443}],
-            https_auth = awsauth, 
-            use_ssl = True,
-            verify_certs = True,
-            connection_class = RequestsHttpConnection
-    )
-    return es
 
 def create_auth():
     credentials = boto3.Session().get_credentials()
