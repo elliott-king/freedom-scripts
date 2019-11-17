ea181af5485122c7f603e066a9a1d42dcb3d6bee
elliott-king <8884571+elliott-king@users.noreply.github.com>
2019-11-17 22:36:54 +0000

    Add ability to snapshot ES domain We will use this to duplicate it to our second ES domain

--- events/es_management.py
+++ events/es_management.py
@@ -5,6 +5,7 @@ from requests_aws4auth import AWS4Auth
 import boto3
 import hashlib
 import time
+import requests
 
 HOST = 'search-freedom-elasti-tajamzilhy9i-a4vp4p7fynyjboefb2qv32fez4.us-east-1.es.amazonaws.com'
 REGION = 'us-east-1'
@@ -40,3 +41,39 @@ def client(host=HOST):
             connection_class = RequestsHttpConnection
     )
     return es
+
+def snapshot_creds():
+    credentials = boto3.Session().get_credentials()
+    return AWS4Auth(
+        credentials.access_key,
+        credentials.secret_key, 
+        REGION, SERVICE, 
+        session_token=credentials.token)
+
+
+def register_snapshot_repo():
+    awsauth = snapshot_creds()
+    host = 'https://' + HOST + '/'
+    path = '_snapshot/my-snapshot-repo'
+    url = host + path
+
+    payload = {
+        'type': 's3',
+        'settings': {
+            'bucket': 'es-freedom-snapshot',
+            'region': REGION,
+            'role_arn': 'arn:aws:iam::888096637450:role/es_snapshot_role'
+        }
+    }
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    return requests.put(url, auth=awsauth, json=payload, headers=headers)
+
+def take_snapshot(name):
+    awsauth = snapshot_creds()
+    host = 'https://' + HOST + '/'
+    path = '_snapshot/my-snapshot-repo/' + name
+    url = host + path
+    return requests.put(url, auth=awsauth)
