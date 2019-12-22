f00f511c41b3de715bfb413887450b1f8ba7b261
elliott-king <8884571+elliott-king@users.noreply.github.com>
2019-12-22 22:29:41 +0000

    Parser for NYC parks site

---
+++ events/nycgovparks.py
@@ -0,0 +1,44 @@
+#  - https://www.nycgovparks.org/events/
+
+import requests
+
+from bs4 import BeautifulSoup
+from datetime import datetime, time
+from dateutil.parser import parse
+
+url = 'https://www.nycgovparks.org'
+page = '/events/free'
+nextpage = '/p' # plus number (counting from 2)
+
+def events():
+    events = []
+    # for suffix in  [''] + [nextpage + str(x) for x in  range(2, 4)]:
+    soup = BeautifulSoup(requests.get(url + page).text, 'html.parser')
+    divs = soup.find_all('div', class_='event')
+
+    for d in divs:
+        body = d.find('div', class_='event_body')
+        image = d.find('div', class_='span2')
+        info = {
+            'name': body.find(class_='event-title').text,
+            'host': 'NYC Parks',
+            'rsvp': False,
+            'times': [
+                str(parse(body.p.find_all('strong')[0].text).time()),
+                str(parse(body.p.find_all('strong')[1].text).time())
+            ],
+            'dates': [str(parse(d.find('div', class_='date_graphic').text).date())],
+            'website': url + body.find(class_='event-title').a.get('href'),
+            'location_description': body.find(class_='location').text,
+            'description': body.find(class_='description').text,
+            'source': url,
+        }
+
+        if info['location_description'][:2] == 'at':
+            info['location_description'] = info['location_description'][3:]
+
+        if image and image.a:
+            info['photos']: [image.a.get('href')]
+
+        events.append(info)
+    return events
