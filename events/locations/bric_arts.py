c6e7d9ebdfba190ab785b6e640f237b0dd6fb87a
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-08-09 01:07:50 +0000

    Move BRIC to their json api

--- events/locations/bric_arts.py
+++ events/locations/bric_arts.py
@@ -1,67 +1,39 @@
 # https://www.bricartsmedia.org/events-performances
 
-import requests
+import requests 
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup 
 from dateutil.parser import parse
-from collections import defaultdict
 
 from scripts import dyn_upload
-from events import utils
 
-base = 'https://www.bricartsmedia.org'
-page = '/events-performances'
-
-# TODO: some events have multiple dates, we are only currently parsing the first!
+base = "https://www.bricartsmedia.org"
+api = "/api/events"
 
 def events(table=dyn_upload.DEV_EVENTS_TABLE):
-    soup = BeautifulSoup(requests.get(base + page).text, 'html.parser')
-    prev_events = defaultdict(list)
-    events = []
-    for article in soup.find_all(class_='node-event'):
-        event = {
-            'name': article.find(class_='listing-title').text,
-            'website': base + article.find(class_='listing-title').a.get('href'),
-            'photos': article.find('img', class_='lazyload').get('src'),
-            'source': 'BRIC',
-            'rsvp': False,
-            'dates': [],
-            'times': [],
-        }
-        if article.find(class_='listing-body').text:
-            event['description'] = article.find(class_='listing-body').text
-
-        event_soup = BeautifulSoup(requests.get(event['website']).text, 'html.parser')
-        for right_item in event_soup.find_all('div', class_='intro-banner-right-item'):
-            if 'Date' in right_item.text:
-                d = right_item.span.text.strip().replace('•', '').replace('|', '')
-                d = d.split('-')[0]
-                if 'am' not in d.lower() and 'pm' not in d.lower():
-                    # TODO: should really check what time it was
-                    # But I am lazy
-                    d += 'pm'
-                try:
-                    d = parse(d)
-                    event['dates'] = [str(d.date())]
-                    event['times'] = [str(d.time())]
-                except Exception as e:
-                    print('Unable to parse:', d)
-            if 'Cost' in right_item.text:
-                # TODO: NEED TO ADD CHECK FOR THIS IN ALL CASES!!!
-                if 'free ' not in right_item.text.lower(): # TODO: 'free' or 'free '?
-                    continue
-                if 'rsvp' in right_item.text.lower() or 'regist' in right_item.text.lower():
-                    event['rsvp'] = True
-            if 'Location' in right_item.text:
-                event['host'] = right_item.strong.text
-                event['location_description'] = right_item.find(class_='thoroughfare').text + ' ' + right_item.find(class_='locality-block').text
-        if 'description' not in event or not event['description']:
-            event['description'] = event_soup.find('div', class_='event-image-contain').find_next_sibling().text
-        for link in event_soup.find_all('a'):
-            if 'rsvp' in link.text.lower() or 'regist' in link.text.lower():
-                event['rsvp'] = True
-
-        if 'host' in event and not dyn_upload.is_uploaded(event, table) and not utils.in_prev_parsed_events(prev_events, event):
-            events.append(event)
-            utils.add_to_prev_events(prev_events, event)
-    return events
+  events = []
+  events_json = requests.get(base + api).json()['events']
+  for i, json in enumerate(events_json):
+    date = parse(json['date']).date().isoformat()
+    # the actual event always seems to be the only element in a subarray
+    json = json['events'][0]
+    if 'free' not in json['cost'].lower():
+      continue
+    event = {
+      'name': json['title'],
+      'website': json['link'],
+      'dates': [date],
+      # WOW this is cheap. Too lazy to immediately fix this
+      'times': [parse(json['time'][:2] + ':' + json['time'][2:]).time().isoformat()],
+      'rsvp': True,
+      'source': 'BRIC',
+      'description': BeautifulSoup(json['desc'], 'html.parser').text.strip(),
+      'host': json['location'] if json['location'] else 'BRIC',
+      # TODO: quarantine only
+      'location_description': 'Brooklyn',
+    }
+    photo_tag = BeautifulSoup(json['image'], 'html.parser').img
+    if photo_tag:
+      event['photos'] = photo_tag['src']
+    events.append(event)
+  return events
